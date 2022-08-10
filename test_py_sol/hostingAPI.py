18b1f5993c536ce67fcf3ccd53b78f412bd957d2
MVSainath <nathsai85@gmail.com>
2022-08-10 14:55:08 +0530

    These are the changes that were to be made originally

--- test_py_sol/hostingAPI.py
+++ test_py_sol/hostingAPI.py
@@ -1,15 +1,9 @@
 import requests
 
-response_API = requests.get(
-    "https://webhook.site/a891e4dc-4056-4d80-8e9a-88e06d98ecd9/"
+url = requests.get(
+    "https://my-json-server.typicode.com/MVSainath/BlockchainEnergyTrading"
 )
-print(response_API.status_code)
 
-url = "https://webhook.site/a891e4dc-4056-4d80-8e9a-88e06d98ecd9/data/"
-
-payload = "{'5','-5','4','3','7','2'}"
-response = requests.request("PUT", url, data=payload)
-
-print(response.text)
+print(url.headers)
 
 '''"'5','-5','4','3','7','2'"'''
